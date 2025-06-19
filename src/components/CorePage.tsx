c3d5481ae040c67bda64f9568abff6a88ce417f9
Cody Garcia <czgarcia@berkeley.edu>
2025-06-19 11:41:52 -0700

    starter core page w/ nav

---
+++ src/components/CorePage.tsx
@@ -0,0 +1,12 @@
+import { Box, Heading, Text } from 'gestalt';
+
+function CorePage() {
+  return (
+    <Box padding={8}>
+      <Heading size="400">Core Page</Heading>
+      <Text>This is the Core page content.</Text>
+    </Box>
+  );
+}
+
+export default CorePage; 
